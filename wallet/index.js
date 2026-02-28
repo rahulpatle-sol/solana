b7aa787dce32a3001c8130a059bcc847b4c5ed9a
rahulpatle-sol <patler044@gmail.com>
2026-02-28 15:31:22 +0530

    connection  rpc

--- wallet/index.js
+++ wallet/index.js
@@ -5,3 +5,5 @@ console.log(`my  public key  is ${keypair.publicKey}`);
 console.log(`privte key  here ${keypair.secretKey}`);
 
 let connection = new Connection("https://api.devnet.solana.com");
+let balance = await connection.getBalance(keypair.publicKey);
+console.log(`balance is ${balance} lamports`);
