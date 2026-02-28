d2df0756b9e60f4872643ea7423218482f1241ec
rahulpatle-sol <patler044@gmail.com>
2026-02-28 15:30:51 +0530

    connection  aded

--- wallet/index.js
+++ wallet/index.js
@@ -3,3 +3,5 @@ import { Keypair } from "@solana/web3.js";
 const  keypair=Keypair.generate();
 console.log(`my  public key  is ${keypair.publicKey}`);
 console.log(`privte key  here ${keypair.secretKey}`);
+
+let connection = new Connection("https://api.devnet.solana.com");
